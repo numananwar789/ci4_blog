188b4280ccef4e3c692a5834add67288c483a041
Numan Anwar <numananwar789@gmail.com>
2023-05-02 11:58:29 +0500

    first commit

---
+++ db/alter.sql
@@ -0,0 +1,18 @@
+CREATE SEQUENCE public."Users_UID_seq";
+
+ALTER SEQUENCE public."Users_UID_seq"
+    OWNER TO ci4_blog_user;
+
+CREATE TABLE public."Users"
+(
+    "UID" bigint NOT NULL DEFAULT nextval('"Users_UID_seq"'::regclass),
+    "Name" character varying(255),
+    "Email" character varying(255),
+    "Password" character varying(50),
+    PRIMARY KEY ("UID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Users"
+    OWNER to ci4_blog_user;
